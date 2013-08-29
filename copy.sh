6c7097354f7e98c308e72c35ca99389bd8f961a5
Mason Malone <mason@masonm.org>
2013-08-29 10:21:06 -0400

    Update copy.sh to echo message if dotfile already exists, and stop exiting with status 1

--- copy.sh
+++ copy.sh
@@ -5,6 +5,10 @@ DOTFILES_DIR="`dirname \"$0\"`"
 for i in $DOTFILES_DIR/*; do
 	[ "$i" = "$0" ] && continue;
 	DOTFILE=".`basename $i`"
+	if [ -a ~/$DOTFILE ]; then
+		echo "$DOTFILE already exists";
+		continue;
+	fi
 	echo "Copying $DOTFILE"
 	cp -n $i ~/$DOTFILE
 done

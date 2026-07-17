8cf81d235790aee19fcdffd6ba1e58b971c47ac6
Mason Malone <651224+MasonM@users.noreply.github.com>
2026-07-16 19:00:07 -0700

    Update to NixOS 26.05
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
